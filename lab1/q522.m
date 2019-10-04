e3a3f599f6a17384e60d8542aff3d9f3f41b257a
freshn <675708295@qq.com>
2019-10-04 15:39:14 +0100

    caxis_setting

---
+++ lab1/q522.m
@@ -0,0 +1,15 @@
+clear;read_pic;
+l1 = -1/8.*[1 1 1;1 -8 1;1 1 1];
+Gmask1 = fspecial('gaussian',[5 5],1.5);
+Gmask2 = fspecial('gaussian',[5 5],5);
+LoGmask1 = conv2(Gmask1,l1,'valid');
+LoGmask2 = conv2(Gmask2,l1,'valid');
+x = conv2(rooster,LoGmask1);
+y = conv2(rooster,LoGmask2);
+
+figure(1);set(gcf,'position',[50,50,800,310]);
+subplot(121),imagesc(x);title('conv from LoG1.5');
+colormap('jet'),colorbar;
+subplot(122),imagesc(y);title('conv from LoG5');
+colormap('jet'),colorbar;
+print -dpng q522.png
