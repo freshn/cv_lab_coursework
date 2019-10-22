743ee17dbd43ee2ea314ea23392d5e84b1e86488
freshn <675708295@qq.com>
2019-10-22 19:50:05 +0100

    new

--- lab1/read_pic.m
+++ lab1/read_pic.m
@@ -3,3 +3,5 @@ rooster = im2double(rooster);
 rooster = rgb2gray(rooster);
 boxes = imread('..\data\boxes.pgm');
 boxes = im2double(boxes);
+ele = imread('..\data\elephant.png');
+ele = im2double(ele);
