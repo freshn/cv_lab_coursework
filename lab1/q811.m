caec8050f49b9a6d27f3677095641d01e3a777a0
freshn <675708295@qq.com>
2019-10-26 22:44:09 +0100

    shower

--- lab1/q811.m
+++ lab1/q811.m
@@ -4,11 +4,11 @@ rt1 = gau_py(rooster);
 rt2 = gau_py(rt1);
 rt3 = gau_py(rt2);
 
-figure(1),set(gcf,'position',[50,50,800,620]);
-subplot(221),imagesc(rooster),colorbar;title('original pic');
-subplot(222),imagesc(rt1),colorbar;title('half pic');
-subplot(223),imagesc(rt2),colorbar;title('1/4 pic');
-subplot(224),imagesc(rt3),colorbar;title('1/8 pic');
+figure(1),set(gcf,'position',[50,50,800,620]);colormap('gray');
+subplot(221),imagesc(rooster),colorbar;title('Gaussian Pyramids 1');
+subplot(222),imagesc(rt1),colorbar;title('Gaussian Pyramids 2');
+subplot(223),imagesc(rt2),colorbar;title('Gaussian Pyramids 3');
+subplot(224),imagesc(rt3),colorbar;title('Gaussian Pyramids 4');
 print -dpng q811.png
 
 function Ia2 = gau_py(Ia1)
--- lab1/q821.m
+++ lab1/q821.m
@@ -7,11 +7,11 @@ rt2_lapl = lapl(rt2);
 rt3 = gau_py(rt2);
 rt3_lapl = lapl(rt3);
 
-figure(1),set(gcf,'position',[50,50,800,620]);
-subplot(221),imagesc(rooster_lapl),colorbar;title('original pic');
-subplot(222),imagesc(rt1_lapl),colorbar;title('half pic');
-subplot(223),imagesc(rt2_lapl),colorbar;title('1/4 pic');
-subplot(224),imagesc(rt3_lapl),colorbar;title('1/8 pic');
+figure(1),set(gcf,'position',[50,50,800,620]);colormap('gray');
+subplot(221),imagesc(rooster_lapl),colorbar;title('Laplacian Pyramids 1');
+subplot(222),imagesc(rt1_lapl),colorbar;title('Laplacian Pyramids 2');
+subplot(223),imagesc(rt2_lapl),colorbar;title('Laplacian Pyramids 3');
+subplot(224),imagesc(rt3_lapl),colorbar;title('Laplacian Pyramids 4');
 print -dpng q821.png
 
 function Ia2 = gau_py(Ia1)
