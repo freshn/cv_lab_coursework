9fc17184f4d9e00925f48631d13aed4fbf0eb63b
freshn <675708295@qq.com>
2019-12-10 16:06:56 +0000

    lab3

---
+++ lab3/example.m
@@ -0,0 +1,9 @@
+clc,clear;
+for i = 3
+    if i <= 9
+        I = imread(['img/train0', num2str(i), '.jpg']);
+    else
+        I = imread(['img/train', num2str(i), '.jpg']);
+    end    
+    [n1, n2] = count_lego(I);
+end
